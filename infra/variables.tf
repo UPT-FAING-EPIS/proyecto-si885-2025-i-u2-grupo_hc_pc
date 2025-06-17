adc2ee385fe188c6266ab26a9ab7902ee822f574
AngelHC <ah2021070017@virtual.upt.pe>
2025-06-17 14:00:30 -0500

    Coreccion 2

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default     = "rg-si885-project-eus2"
+  default     = "rg-si885-project-final"
 }
 
 variable "sql_server_name" {
