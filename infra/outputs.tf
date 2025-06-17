9d56d418e9c273e5c4228b408a09bbb086eae9a9
AngelHC <ah2021070017@virtual.upt.pe>
2025-06-17 15:57:45 -0500

    test4

---
+++ infra/outputs.tf
@@ -0,0 +1,19 @@
+output "connection_string" {
+  value     = azurerm_storage_account.storage.primary_connection_string
+  sensitive = true
+}
+
+output "sql_server_fqdn" {
+  description = "The fully qualified domain name of the SQL server."
+  value       = azurerm_mssql_server.sqlserver.fully_qualified_domain_name
+}
+
+output "sql_database_name" {
+  description = "The name of the SQL database."
+  value       = azurerm_mssql_database.db.name
+}
+
+output "sql_admin_login" {
+  description = "The admin login for the SQL server."
+  value       = azurerm_mssql_server.sqlserver.administrator_login
+}
